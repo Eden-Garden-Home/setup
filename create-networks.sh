ba54a5ceeb4e7f9772c46922b8a6d52b7d6dab14
aleeric <aleric.machine@gmail.com>
2025-10-16 18:54:39 +0200

    feat(tailscale-internal): network for tailscale internal created

--- create-networks.sh
+++ create-networks.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Creazione delle reti Docker per la segregazione
-echo "Creazione reti Docker..."
+echo "I'm creating networks for docker"
 
-# Rete pubblica per Traefik
 docker network create traefik-public --driver bridge --subnet=172.20.0.0/16
-
-# Rete interna per Authentik
 docker network create authentik-internal --driver bridge --subnet=172.21.0.0/16 --internal
-
-# Rete interna per i servizi
 docker network create services-internal --driver bridge --subnet=172.22.0.0/16 --internal
-
-# Rete interna per i database
 docker network create database-internal --driver bridge --subnet=172.23.0.0/16 --internal
+docker network create tailscale-internal --driver bridge --subnet=172.24.0.0/16 --internal
 
-echo "Reti create con successo!"
+
+
+echo "Done!"
 echo ""
-echo "Reti disponibili:"
+echo "Network available"
 docker network ls | grep -E "(traefik-public|authentik-internal|services-internal|database-internal)"
